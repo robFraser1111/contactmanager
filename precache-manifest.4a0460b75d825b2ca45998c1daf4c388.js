aa5fc530ce9a492f9fdceea0e235b9abd04fb392
Rob <robfraserdesign@gmail.com>
2020-07-24 17:58:53 +1000

    Updates

---
+++ precache-manifest.4a0460b75d825b2ca45998c1daf4c388.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b37e845ccd9d3f742cd5f1c126ad62",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "d2e9a8245fd9a4454049",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2d1f0e4e40ba171460a8",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d2e9a8245fd9a4454049",
+    "url": "/contactmanager/static/js/2.37624af6.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/contactmanager/static/js/2.37624af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1f0e4e40ba171460a8",
+    "url": "/contactmanager/static/js/main.2ae9d42e.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
